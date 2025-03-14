68b8a8010378c9ef4a91754fd90aee52e65e4821
aliko <gracealiko09@gmail.com>
2025-03-14 23:40:10 +0000

    started sidebar and header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-white shadow p-4 flex justify-between items-center">
+      <h1 className="text-lg font-semibold">Church Management</h1>
+      <button className="px-4 py-2 bg-red-500 text-white rounded-lg">
+        Logout
+      </button>
+    </header>
+  );
 };
 
 export default Header;
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Header</div>;
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <nav
+      className={`flex flex-col md:block bg-[#071739] h-full text-[#CDD5DB] font-semibold border border-l-2 border-Primary-light ${
+        isCollapsed ? "w-16" : "w-[180px]"
+      }`}>
+      <div>
+        <button
+          className=" w-full px-4 py-5 text-Primary hover:bg-custom-brown hover:text-Primary"
+          onClick={toggleSidebar}>
+          {isCollapsed ? (
+            <svg className="w-4 h-4">
+              <use href="/sprite.svg#icon-circle-right"></use>
+            </svg>
+          ) : (
+            <svg className="w-4 h-4">
+              <use href="/sprite.svg#icon-circle-left"></use>
+            </svg>
+          )}
+        </button>
+        <ul className="flex-grow">
+          <li className="flex items-center gap-2 px-4 py-5 hover:bg-custom-brown hover:text-Primary">
+            <svg className="w-4 h-4">
+              <use href="/sprite.svg#icon-home3"></use>
+            </svg>
+            {!isCollapsed && <NavLink to="/dashboard">Home</NavLink>}
+          </li>
+          <li className="flex items-center gap-2 px-4 py-5 hover:bg-custom-brown hover:text-Primary">
+            <svg className="w-4 h-4">
+              <use href="/sprite.svg#icon-users"></use>
+            </svg>
+            {!isCollapsed && <NavLink to="/members">Members</NavLink>}
+          </li>
+          <li className="flex items-center gap-2 px-4 py-5 hover:bg-custom-brown hover:text-Primary">
+            <svg className="w-4 h-4">
+              <use href="/sprite.svg#icon-library"></use>
+            </svg>
+            {!isCollapsed && <NavLink to="/branches">Departments</NavLink>}
+          </li>
+        </ul>
+      </div>
+      <button className="mt-auto w-full flex items-center gap-2 px-4 py-5 cursor-pointer hover:bg-custom-brown hover:text-Primary">
+        <svg className="w-4 h-4">
+          <use href="/sprite.svg#icon-share"></use>
+        </svg>
+        {/* {!isCollapsed && <Link to="/dashboard/logout">LOG OUT</Link>} */}
+      </button>
+    </nav>
+  );
 };
 
 export default Sidebar;
